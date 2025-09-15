2efc55d132bb8746c0deef2953f1bb666dbf4a85
krisna <krisna.w2010@gmail.com>
2025-09-15 17:02:49 +0800

    init

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import {InvoiceContainer} from "@/components/invoice/invoice-container";
+import {InvoiceProvider} from "@/components/invoice/invoice-provider";
 
 export default function Page() {
   return (
-    <div>
+    <InvoiceProvider>
       <InvoiceContainer />
-    </div>
+    </InvoiceProvider>
   )
 }
--- components/invoice/invoice-container.tsx
+++ components/invoice/invoice-container.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {InvoiceContext, InvoiceProvider} from "@/components/invoice/invoice-provider";
+import {InvoiceContext} from "@/components/invoice/invoice-provider";
 import {InvoiceForm} from "@/components/invoice/invoice-form";
 import {useContext} from "react";
 
@@ -7,19 +7,21 @@ export function InvoiceContainer() {
   const {issueDate} = useContext(InvoiceContext);
   const {invoiceNumber} = useContext(InvoiceContext);
   return (
-    <InvoiceProvider>
+    <>
       <div>
-        Provider Data
-        <ul>
-          <li>
-            Invoice Number: {invoiceNumber}
-          </li>
-          <li>
-            Issue Date: {issueDate?.toLocaleDateString()}
-          </li>
-        </ul>
+        <div>
+          Provider Data
+          <ul>
+            <li>
+              Invoice Number: {invoiceNumber}
+            </li>
+            <li>
+              Issue Date: {issueDate?.toLocaleDateString()}
+            </li>
+          </ul>
+        </div>
+        <InvoiceForm />
       </div>
-      <InvoiceForm />
-    </InvoiceProvider>
+    </>
   )
 }
