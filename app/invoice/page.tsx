e6178c30b4b41da95bbb37f1b22250d922512c6a
krisna <krisna.w2010@gmail.com>
2025-09-15 12:12:13 +0800

    init

---
+++ app/invoice/page.tsx
@@ -0,0 +1,9 @@
+import {CreateInvoice} from "@/components/create-invoice";
+
+export default function Page() {
+  return (
+    <div>
+      <CreateInvoice />
+    </div>
+  )
+}
---
+++ components/create-invoice.tsx
@@ -0,0 +1,7 @@
+export function CreateInvoice() {
+  return (
+    <div>
+      sdf
+    </div>
+  )
+}
