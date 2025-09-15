c7bde739f6810cbfbe196f75737ab07a94b4b110
krisna <krisna.w2010@gmail.com>
2025-09-15 12:24:35 +0800

    init

--- components/create-invoice.tsx
+++ components/create-invoice.tsx
@@ -1,7 +1,44 @@
 export function CreateInvoice() {
   return (
-    <div>
-      sdf
+    <div className="mx-auto max-w-5xl min-h-screen flex justify-center items-center py-10 font-mono">
+      <div className="border border-gray-300 shadow-lg h-full w-full p-10">
+
+        <div>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="font-mono">
+              Invoice no: INV-0001
+            </div>
+
+            <div>
+              Issue date: 2024-01-01
+            </div>
+
+            <div>
+              Due date: 2024-01-31
+            </div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-4 mt-10">
+            <div>
+              From
+            </div>
+
+            <div>
+              To
+            </div>
+          </div>
+        </div>
+
+        <div>
+          Body
+        </div>
+
+        <div>
+          Footer
+        </div>
+
+
+      </div>
     </div>
   )
 }
