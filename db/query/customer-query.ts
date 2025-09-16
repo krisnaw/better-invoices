dc57c704c4b095810d055f7e5b52009861282c16
krisna <krisna.w2010@gmail.com>
2025-09-16 11:17:20 +0800

    wip auth

--- db/query/customer-query.ts
+++ db/query/customer-query.ts
@@ -2,6 +2,10 @@ import {db} from "@/db/db-connection";
 import {customersTable} from "@/db/schema/customer-schema";
 import {eq} from "drizzle-orm";
 
+export async function getCustomersByUserId(userId: string) {
+  return db.select().from(customersTable).where(eq(customersTable.userId, userId)).limit(10);
+}
+
 export async function getCustomerById(id: number) {
   const customer = await db.select().from(customersTable).where(eq(customersTable.id, id)).limit(1);
   return customer[0]
