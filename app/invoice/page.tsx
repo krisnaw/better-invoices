c240d224f457e3cff49da3f813784ef99f52a197
krisna <krisna.w2010@gmail.com>
2025-09-18 13:37:19 +0800

    wip auth

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -1,7 +1,20 @@
-import {InvoiceContainer} from "@/components/invoice/invoice-container";
+"use client"
+import nextDynamic from "next/dynamic";
+import {InvoiceTemplate} from "@/lib/invoice-template";
+
+export const dynamic = "force-dynamic";
+
+const PDFViewer = nextDynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {ssr: false},
+);
 
 export default function Page() {
   return (
-    <InvoiceContainer />
-  )
+    <div className="h-screen w-full">
+      <PDFViewer>
+        <InvoiceTemplate />
+      </PDFViewer>
+    </div>
+  );
 }
