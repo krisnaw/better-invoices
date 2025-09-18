c3634ff2919cc20a24adb69bde851f322d54b1e3
krisna <krisna.w2010@gmail.com>
2025-09-18 13:23:14 +0800

    wip auth

--- lib/invoice-template.tsx
+++ lib/invoice-template.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
-import {Document, Page, StyleSheet, Text, View} from '@react-pdf/renderer';
+import {Document, Font, Page, StyleSheet, Text, View} from '@react-pdf/renderer';
+
+Font.register({
+  family: "Inter",
+  fonts: [
+    {
+      src: "https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfMZhrib2Bg-4.ttf",
+      fontWeight: 400,
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuI6fMZhrib2Bg-4.ttf",
+      fontWeight: 500,
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuGKYMZhrib2Bg-4.ttf",
+      fontWeight: 600,
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v12/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuFuYMZhrib2Bg-4.ttf",
+      fontWeight: 700,
+    },
+    // Italic fonts
+    {
+      src: "https://fonts.gstatic.com/s/inter/v19/UcCM3FwrK3iLTcvneQg7Ca725JhhKnNqk4j1ebLhAm8SrXTc2dthjQ.ttf",
+      fontWeight: 400,
+      fontStyle: "italic",
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v19/UcCM3FwrK3iLTcvneQg7Ca725JhhKnNqk4j1ebLhAm8SrXTc69thjQ.ttf",
+      fontWeight: 500,
+      fontStyle: "italic",
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v19/UcCM3FwrK3iLTcvneQg7Ca725JhhKnNqk4j1ebLhAm8SrXTcB9xhjQ.ttf",
+      fontWeight: 600,
+      fontStyle: "italic",
+    },
+    {
+      src: "https://fonts.gstatic.com/s/inter/v19/UcCM3FwrK3iLTcvneQg7Ca725JhhKnNqk4j1ebLhAm8SrXTcPtxhjQ.ttf",
+      fontWeight: 700,
+      fontStyle: "italic",
+    },
+  ],
+});
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    padding: 20,
+    backgroundColor: "#fff",
+    color: "#000",
+    fontFamily: "Inter",
+    fontWeight: 400,
   },
   section: {
     margin: 10,
@@ -18,12 +64,15 @@ const styles = StyleSheet.create({
 export  const InvoiceTemplate = () => (
   <Document>
     <Page size="A4" style={styles.page}>
+
       <View style={styles.section}>
         <Text>Section #1</Text>
       </View>
+
       <View style={styles.section}>
         <Text>Section #2</Text>
       </View>
+
     </Page>
   </Document>
 );
