e7563c91f8c3489c838c06b2c6a87f3db54f2c88
krisna <krisna.w2010@gmail.com>
2025-09-25 17:22:47 +0800

    added middleware

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -7,10 +7,48 @@ import {Textarea} from "@/components/ui/textarea";
 import {Button} from "@/components/ui/button";
 import {Plus, Trash} from "lucide-react";
 
+const projects = [
+   {
+      id: 1,
+      name: 'Logo redesign',
+      description: 'New logo and digital asset playbook.',
+      hours: '20.0',
+      rate: '$100.00',
+      price: '$2,000.00',
+   },
+   {
+      id: 2,
+      name: 'Website redesign',
+      description: 'Design and program new company website.',
+      quantity: '52.0',
+      rate: '$100.00',
+      price: '$5,200.00',
+   },
+   {
+      id: 3,
+      name: 'Business cards',
+      description: 'Design and production of 3.5" x 2.0" business cards.',
+      hours: '12.0',
+      rate: '$100.00',
+      price: '$1,200.00',
+   },
+   {
+      id: 4,
+      name: 'T-shirt design',
+      description: 'Three t-shirt design concepts.',
+      hours: '4.0',
+      rate: '$100.00',
+      price: '$400.00',
+   },
+]
+
+
 type LineItem = {
+   name: string,
    description: string;
    quantity: number;
-   unitPrice: number;
+   rate: number;
+   price: string;
 }
 
 type InvoiceType = {
@@ -21,16 +59,26 @@ type InvoiceType = {
 }
 
 export default function Page() {
-   const [from, setFrom] = useState<string>("")
-   const [customer, setCustomer] = useState<string>("")
-   const [invoiceNumber, setInvoiceNumber] = useState<string>("")
-   const [lineItems, setLineItems] = useState<LineItem[]>([])
+   const [from, setFrom] = useState<string>("Acme Inc.")
+   const [customer, setCustomer] = useState<string>("Customer")
+   const [invoiceNumber, setInvoiceNumber] = useState<string>("INV-00001")
+   const [lineItems, setLineItems] = useState<LineItem[]>([
+      {
+         name: 'Website redesign',
+         description: 'Design and program new company website.',
+         quantity: 1,
+         rate: 1,
+         price: '$5,200.00',
+      },
+   ])
 
    const onClickAddLineItem = () => {
       const newItem : LineItem = {
+         name: "product name",
          description: "Type new product" + (lineItems.length + 1) + " here",
          quantity: 0,
-         unitPrice: 0,
+         rate: 0,
+         price: "100",
       }
       setLineItems((prevState) => [newItem, ...lineItems])
    }
@@ -42,154 +90,139 @@ export default function Page() {
    }
 
    return (
-       <div className="h-screen w-full grid grid-cols-2 p-4 gap-x-2">
+       <div className="h-screen mx-auto max-w-7xl p-4 gap-x-2">
           <div className="divide-y divide-gray-200 overflow-scroll rounded-lg bg-white shadow-sm">
              <div className="px-4 py-5 sm:px-6">
 
                 {/*HEADER*/}
                 <div className=" grid grid-cols-2 gap-4">
-                   <div className="col-span-2">
+                   <div className="col-span-1">
                       <div className="font-medium text-gray-900">
-                         Invoice Number
+                         Invoice number
                       </div>
                       <div className="mt-1.5">
                          <Input value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value)} />
                       </div>
                    </div>
 
-                   <div className="col-span-2 grid grid-cols-2 gap-x-4">
-                      <div>
-                         <div className="font-medium text-gray-900">
-                            From
-                         </div>
-                         <div className="mt-1.5">
-                            <Textarea className="w-full" value={from} onChange={(e) => setFrom(e.target.value)} />
-                         </div>
+                   <div className="col-span-1">
+                      <div className="font-medium text-gray-900">
+                         Invoice date
                       </div>
-
-                      <div>
-                         <div className="font-medium text-gray-900">
-                            Customer
-                         </div>
-                         <div className="mt-1.5">
-                            <Select
-                                value={customer} onValueChange={(value: string) => setCustomer(value)}>
-                               <SelectTrigger className="w-full">
-                                  <SelectValue placeholder="Theme" />
-                               </SelectTrigger>
-                               <SelectContent>
-                                  <SelectItem value="light">Light</SelectItem>
-                                  <SelectItem value="dark">Dark</SelectItem>
-                                  <SelectItem value="system">System</SelectItem>
-                               </SelectContent>
-                            </Select>
-
-                         </div>
+                      <div className="mt-1.5">
+                         <Input value={invoiceNumber} onChange={(e) => setInvoiceNumber(e.target.value)} />
                       </div>
+                   </div>
 
+                   <div className="col-span-1">
+                      <div className="font-medium text-gray-900">
+                         From
+                      </div>
+                      <div className="mt-1.5">
+                         <Textarea className="w-full" value={from} onChange={(e) => setFrom(e.target.value)} />
+                      </div>
+                   </div>
+                   <div className="col-span-1">
+                      <div className="font-medium text-gray-900">
+                         Customer
+                      </div>
+                      <div className="mt-1.5">
+                         <Select
+                             value={customer} onValueChange={(value: string) => setCustomer(value)}>
+                            <SelectTrigger className="w-full">
+                               <SelectValue placeholder="Theme" />
+                            </SelectTrigger>
+                            <SelectContent>
+                               <SelectItem value="light">Light</SelectItem>
+                               <SelectItem value="dark">Dark</SelectItem>
+                               <SelectItem value="system">System</SelectItem>
+                            </SelectContent>
+                         </Select>
+
+                      </div>
                    </div>
                 </div>
 
 
              </div>
-             <div className="px-4 py-5 sm:p-6 space-y-2">
 
-                <div>
-                   <div>
-                      <table className="relative min-w-full divide-y divide-gray-300">
-                         <thead>
-                         <tr>
-                            <th scope="col" className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">
-                               Description
-                            </th>
-                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                               Quantity
-                            </th>
-                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                               Price
-                            </th>
-                            <th scope="col" className="py-3.5 pr-4 pl-3 sm:pr-0">
-                               <span className="sr-only">Edit</span>
-                            </th>
-                         </tr>
-                         </thead>
-                         <tbody className="divide-y divide-gray-200 max-h-36 overflow-scroll">
-                         {lineItems.map((item, index) => (
-                             <tr key={index}>
-                                <td className="py-4 pr-3 pl-4 text-sm font-medium whitespace-nowrap text-gray-900 sm:pl-0">
-                                   {item.description}
-                                </td>
-                                <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
-                                   {item.quantity}
-                                </td>
-                                <td className="px-3 py-4 text-sm whitespace-nowrap text-gray-500">
-                                   {item.unitPrice}
-                                </td>
+             {/*MAIN*/}
+             <div className="px-4 sm:px-6 lg:px-8 pb-2.5">
+                <div className="-mx-4 mt-8 flow-root sm:mx-0">
+                   <table className="min-w-full">
+                      <colgroup>
+                         <col className="w-full sm:w-1/2" />
+                         <col className="sm:w-1/6" />
+                         <col className="sm:w-1/6" />
+                         <col className="sm:w-1/6" />
+                      </colgroup>
+                      <thead className="border-b border-gray-300 text-gray-900">
+                      <tr>
+                         <th scope="col" className="py-3.5 pr-3 pl-4 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                            Description
+                         </th>
+                         <th scope="col" className="hidden px-3 py-3.5 text-right text-sm font-semibold text-gray-900 sm:table-cell">
+                            Quantity
+                         </th>
+                         <th scope="col" className="py-3.5 pr-4 pl-3 text-right text-sm font-semibold text-gray-900 sm:pr-2">
+                            Price
+                         </th>
+                         <th>
+                            <span className="sr-only">Actions</span>
+                         </th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {lineItems.map((project, index) => (
+                          <tr key={index} className="border-b border-gray-200">
 
-                                <td className="py-4 pr-4 pl-3 text-right text-sm font-medium whitespace-nowrap sm:pr-0">
-                                   <Button variant="destructive" size="icon" onClick={() => onClickRemoveLineItem(index)}>
-                                      <Trash />
-                                   </Button>
-                                </td>
-                             </tr>
-                         ))}
-                         </tbody>
-                         <tfoot>
-                         <tr>
-                            <td colSpan={5}>
-                               <div className="flex justify-end mt-4">
-                                  <Button size="icon" onClick={onClickAddLineItem}>
-                                     <Plus />
-                                  </Button>
-                               </div>
-                            </td>
-                         </tr>
-                         </tfoot>
-                      </table>
-                   </div>
+                             <td className="max-w-0 py-5 pr-3 pl-4 text-sm sm:pl-0">
+                                <div className="font-medium text-gray-900">
+                                   <Input type="text" name="project_name" defaultValue={project.name}  />
+                                </div>
+                                <div className="mt-1 truncate text-gray-500">
+                                   <Input type="text" name="project_description" defaultValue={project.description} />
+                                </div>
+                             </td>
 
+                             <td className="hidden px-3 py-5 text-right text-sm text-gray-500 sm:table-cell">
+                                <Input type="number" name="project_quantity" defaultValue={project.quantity} />
+                             </td>
 
+                             <td className="py-5 pr-4 pl-3 text-right text-sm text-gray-500 sm:pr-2">
+                                <Input type="number" name="project_rate" defaultValue={project.price} />
+                             </td>
+
+                             <td>
+                                <Button variant="ghost" size="icon"
+                                        onClick={() => onClickRemoveLineItem(index)}>
+                                   <Trash />
+                                </Button>
+                             </td>
+                          </tr>
+                      ))}
+                      </tbody>
+                      <tfoot>
+                      <tr>
+                         <th scope="row" className="pt-6 pr-3 pl-4 text-left text-sm font-normal text-gray-500 sm:hidden">
+                            Actions
+                         </th>
+                         <td colSpan={5} className="pt-6 pr-4 pl-3 text-right text-sm text-gray-500 sm:pr-0">
+                            <Button size="icon" onClick={onClickAddLineItem}>
+                               <Plus />
+                            </Button>
+                         </td>
+                      </tr>
+                      </tfoot>
+                   </table>
                 </div>
-
-
              </div>
+
              <div className="px-4 py-4 sm:px-6">
                 {/* Content goes here */}
                 {/* We use less vertical padding on card footers at all sizes than on headers or body sections */}
              </div>
-          </div>
 
-
-
-          <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-gray-200 shadow-sm">
-             <div className="px-4 py-5 sm:px-6">
-                Preview
-             </div>
-             <div className="px-4 py-5 sm:p-6">
-                <div>
-                   <div className="font-medium text-gray-900">
-                      {invoiceNumber}
-                   </div>
-
-
-                   <div className="grid grid-cols-2 gap-x-4">
-
-                      <div className="font-medium text-gray-900">
-                         {from}
-                      </div>
-
-                      <div className="font-medium text-gray-900">
-                         {customer}
-                      </div>
-
-                   </div>
-
-                </div>
-             </div>
-             <div className="px-4 py-4 sm:px-6">
-                {/* Content goes here */}
-                {/* We use less vertical padding on card footers at all sizes than on headers or body sections */}
-             </div>
           </div>
        </div>
    )
