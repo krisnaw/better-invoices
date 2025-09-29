e49e78bad22b8838864883b8dc3ebb2a6c2753b8
krisna <krisna.w2010@gmail.com>
2025-09-29 18:25:34 +0800

    added middleware

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,4 @@
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card"
+import {Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card"
 import {getCustomersByUserId} from "@/db/query/customer-query";
 import {auth} from "@/lib/auth";
 import {headers} from "next/headers";
@@ -16,15 +16,32 @@ export default async function Customer() {
   }
   const customers = await getCustomersByUserId(session.user.id);
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Customers</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Suspense fallback={<CustomerListSkeleton />}>
-          <CustomerList customers={customers} />
-        </Suspense>
-      </CardContent>
-    </Card>
+      <div>
+         <Card>
+            <CardHeader>
+               <CardTitle>Card Title</CardTitle>
+               <CardDescription>Card Description</CardDescription>
+               <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+               <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+               <p>Card Footer</p>
+            </CardFooter>
+         </Card>
+
+
+         <Card>
+            <CardHeader>
+               <CardTitle>Customers</CardTitle>
+            </CardHeader>
+            <CardContent>
+               <Suspense fallback={<CustomerListSkeleton />}>
+                  <CustomerList customers={customers} />
+               </Suspense>
+            </CardContent>
+         </Card>
+      </div>
   )
 }
