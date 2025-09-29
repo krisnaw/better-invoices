9b5ed578ea757e454ec4cafa9b78dd4a1c05cecc
krisna <krisna.w2010@gmail.com>
2025-09-29 18:57:35 +0800

    added middleware

--- components/customer/customer-item.tsx
+++ components/customer/customer-item.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import Link from "next/link"
-
 import {CustomerType} from "@/lib/types"
 import {TableCell, TableRow} from "@/components/ui/table"
 import {CustomerActions} from "@/components/customer/customer-actions"
