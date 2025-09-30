3b4ebb86652bf5d87ab742e73443a0d3ac46a383
krisna <krisna.w2010@gmail.com>
2025-09-30 16:16:11 +0800

    added middleware

--- components/auth/forgot-password.tsx
+++ components/auth/forgot-password.tsx
@@ -3,69 +3,61 @@ import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/compo
 import {Label} from "@/components/ui/label";
 import {Input} from "@/components/ui/input";
 import {Button} from "@/components/ui/button";
-import {authClient} from "@/lib/auth-client";
-import {useState} from "react";
-import {toast} from "sonner";
+
 import {Loader2} from "lucide-react";
+import {useActionState} from "react";
+import {forgotPasswordAction} from "@/app/actions/auth/forgot-password.action";
+import {ActionResponse} from "@/lib/types";
+import {toast} from "sonner";
 
 export default function ForgotPassword() {
-  const [email, setEmail] = useState("");
-  const [loading, setLoading] = useState(false);
-  const onClickForgotPassword = async () => {
-    if (!email) {
-      toast.error("Please enter your email address.");
-      return;
+
+  const [state, formAction, loading] = useActionState<ActionResponse, FormData>(async (prevState, formData: FormData) => {
+    const email = formData.get("email") as string;
+    const submitData = {
+      email: email,
     }
-
-    setLoading(true);
-
-    try {
-      const {error} = await authClient.requestPasswordReset({
-        email,
-        redirectTo: "http://localhost:3000/reset-password",
-      });
-
-      if (error) {
-        toast.error(error.message ?? "Something went wrong. Try again later.");
-        return;
-      }
-
-      toast.success("Check your email for a link to reset your password.");
-    } finally {
-      setLoading(false);
+    const res = await forgotPasswordAction(submitData)
+    if (!res.success) {
+      toast.error(res.message)
+    } else {
+      toast.success(res.message)
     }
-  };
+    return res
+  }, {
+    success: false,
+    message: ""
+  })
 
   return (
-    <Card className="min-w-sm">
-      <CardHeader>
-        <CardTitle className="text-lg md:text-xl">Forgot password</CardTitle>
-        <CardDescription className="text-xs md:text-sm">
-          Enter your email below to reset your password.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              value={email}
-            />
-          </div>
+    <form action={formAction}>
+      <Card className="min-w-sm">
+        <CardHeader>
+          <CardTitle className="text-lg md:text-xl">Forgot password</CardTitle>
+          <CardDescription className="text-xs md:text-sm">
+            Enter your email below to reset your password.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
 
-          <Button type="submit" onClick={onClickForgotPassword} className="w-full" disabled={loading}>
-            Reset password
-            {loading && <Loader2 className="animate-spin" />}
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+            <Button type="submit" className="w-full" disabled={loading}>
+              Reset password
+              {loading && <Loader2 className="animate-spin" />}
+            </Button>
+          </div>
+        </CardContent>
+      </Card>
+    </form>
   )
 }
