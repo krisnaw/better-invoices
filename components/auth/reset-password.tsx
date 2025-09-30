006faeb227e11d82a755fd3a77c57d14a3ca3204
krisna <krisna.w2010@gmail.com>
2025-09-30 16:17:22 +0800

    added middleware

--- components/auth/reset-password.tsx
+++ components/auth/reset-password.tsx
@@ -1,72 +1,69 @@
 "use client"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {Label} from "@/components/ui/label";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {authClient} from "@/lib/auth-client";
-import {useState} from "react";
-import {useRouter, useSearchParams} from "next/navigation";
+
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {useActionState} from "react";
+import {ActionResponse} from "@/lib/types";
+import {changePasswordAction} from "@/app/actions/auth/change-password.action";
 import {toast} from "sonner";
+import {useRouter} from "next/navigation";
+import {Button} from "@/components/ui/button";
 import {Loader2} from "lucide-react";
 
-export default function ResetPassword() {
-  const [password, setPassword] = useState("");
-  const router = useRouter();
-  const searchParams = useSearchParams()
-  const [loading, setLoading] = useState(false)
-  const token = searchParams.get('token')
-
-  if (!token) {
-    return <div>Invalid token</div>
-  }
-
-  const onClickForgotPassword = async () => {
-    if (!password) {
-      toast.error("Please enter a new password")
-      return
+export default function ResetPassword({token}: { token: string }) {
+  const router = useRouter()
+  const [state, formAction, isPending] = useActionState<ActionResponse, FormData>(async (prevState, formData: FormData) => {
+    const submitData = {
+      newPassword: formData.get("password") as string,
+      token: formData.get("token") as string,
     }
-
-    setLoading(true)
-    await authClient.resetPassword({
-      newPassword: password,
-      token: String(token),
-    })
-    toast.success("Password reset successfully")
-    setLoading(false)
+    const res = await changePasswordAction(submitData)
+    if (!res.success) {
+      toast.error(res.message)
+    }
+    toast.success(res.message)
     router.push("/login")
-  };
+    return res;
+  }, {
+    success: false,
+    message: ""
+  })
 
   return (
-    <Card className="min-w-sm">
-      <CardHeader>
-        <CardTitle className="text-lg md:text-xl">Forgot password</CardTitle>
-        <CardDescription className="text-xs md:text-sm">
-          Enter your email below to reset your password.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">New password</Label>
+    <form action={formAction}>
+      <input type="hidden" name="token" value={token} readOnly/>
+      <Card className="min-w-sm">
+        <CardHeader>
+          <CardTitle className="text-lg md:text-xl">Forgot password</CardTitle>
+          <CardDescription className="text-xs md:text-sm">
+            Enter your email below to reset your password.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">New password</Label>
+              </div>
+
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="password"
+                autoComplete="new-password"
+                required
+              />
             </div>
 
-            <Input
-              id="password"
-              type="password"
-              placeholder="password"
-              autoComplete="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <Button type="submit" className="w-full" disabled={isPending}>
+              Reset password
+              {isPending ? <Loader2 className="animate-spin"/> : null}
+            </Button>
           </div>
-
-          <Button type="submit" onClick={onClickForgotPassword} className="w-full" disabled={loading}>
-            Reset password
-            {loading && <Loader2 className="animate-spin" />}
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </form>
   )
 }
