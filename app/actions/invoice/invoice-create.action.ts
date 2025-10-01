c27a8078cd9ed63cf6f35ea47014c3ecb7441fb8
krisna <krisna.w2010@gmail.com>
2025-10-01 12:31:14 +0800

    wip create invoice

--- app/actions/invoice/invoice-create.action.ts
+++ app/actions/invoice/invoice-create.action.ts
@@ -1,8 +1,39 @@
 "use server"
 
 import {ActionResponse} from "@/lib/types";
+import {db} from "@/db/db-connection";
 
 export async function createInvoiceAction(): Promise<ActionResponse> {
+
+  await db.transaction(async (tx) => {
+    // const [invoice] = await tx.insert(invoicesTable).values({
+    //   userId: '',
+    //   customerId: 1,
+    //   invoiceNumber: "INV-001",
+    //   status: "paid",
+    //   currency: "USD",
+    //   description: "Test invoice",
+    // }).returning()
+
+    // await tx.insert(invoiceItemsTable).values(
+    //   [
+    //     {
+    //       invoiceId: invoice.id,
+    //       name: "Test item",
+    //       quantity: 1,
+    //       price: '10000',
+    //     },
+    //     {
+    //       invoiceId: invoice.id,
+    //       name: "Test item",
+    //       quantity: 1,
+    //       price: '10000',
+    //     }
+    //   ]
+    // )
+
+  });
+
   return {
     success: true,
     message: "Invoice created successfully",
