e4a5f4cec0f3717b27b1beab7f737e477b873914
krisna <krisna.w2010@gmail.com>
2025-10-01 12:02:45 +0800

    added webhook

--- app/actions/send.action.ts
+++ app/actions/send.action.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendAction(): Promise<void> {
   const { data, error } = await resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
-    to: ['delivered@resend.dev'],
+    to: ['krisna.w2010@gmail.com'],
     subject: 'Hello world',
     react: InvoiceEmail({ firstName: 'John' }),
     attachments: [
